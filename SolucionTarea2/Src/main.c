7f344d59cad66a928453940c8461ca38fd3c5b09
Druizv <daruizve@unal.edu.co>
2023-03-20 11:43:36 -0500

    cambios

--- SolucionTarea2/Src/main.c
+++ SolucionTarea2/Src/main.c
@@ -56,7 +56,7 @@ int main(void)
 	// Deseamos trabajar con el puerto GPIOA
 	handlerUserLedPin1.pGPIOx 	 				  			=	GPIOA;
 	handlerUserLedPin1.GPIO_PinConfig.GPIO_PinNumber 		=	PIN_1;
-	handlerUserLedPin1.GPIO_PinConfig.GPIO_PinMode	 		=	GPIO_MODE_OUT;
+	handlerUserLedPin1.GPIO_PinConfig.GPIO_PinMode	 		=	GPIO_MODE_IN;
 	handlerUserLedPin1.GPIO_PinConfig.GPIO_PinOType	 		=	GPIO_OTYPE_PUSHPULL;
 	handlerUserLedPin1.GPIO_PinConfig.GPIO_PinPuPdControl	=	GPIO_PUPDR_PULLUP;
 	handlerUserLedPin1.GPIO_PinConfig.GPIO_PinSpeed			=	GPIO_OSPEED_MEDIUM;
@@ -81,7 +81,7 @@ int main(void)
 	// primero configuramos el botón
 	handlerUserButton.pGPIOx 								= GPIOC;
 	handlerUserButton.GPIO_PinConfig.GPIO_PinNumber			= PIN_13;
-	handlerUserButton.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_IN;
+	handlerUserButton.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
 	handlerUserButton.GPIO_PinConfig.GPIO_PinOType			= GPIO_OTYPE_PUSHPULL;
 	handlerUserButton.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
 	handlerUserButton.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_FAST;
@@ -95,7 +95,7 @@ int main(void)
 	// configurando el bit-0 puerto A (GPIOA) pin 7 PA7
 	handlerLED_1.pGPIOx										= GPIOA;
 	handlerLED_1.GPIO_PinConfig.GPIO_PinNumber				= PIN_7;
-	handlerLED_1.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_IN;
+	handlerLED_1.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
 	handlerLED_1.GPIO_PinConfig.GPIO_PinOType				= GPIO_OTYPE_PUSHPULL;
 	handlerLED_1.GPIO_PinConfig.GPIO_PinPuPdControl			= GPIO_PUPDR_NOTHING;
 	handlerLED_1.GPIO_PinConfig.GPIO_PinSpeed				= GPIO_OSPEED_FAST;
@@ -104,7 +104,7 @@ int main(void)
 	//configurando el bit-1 puerto C (GPIOC) pin 8 PC8
 	handlerLED_2.pGPIOx										= GPIOC;
 	handlerLED_2.GPIO_PinConfig.GPIO_PinNumber				= PIN_8;
-	handlerLED_2.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_IN;
+	handlerLED_2.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
 	handlerLED_2.GPIO_PinConfig.GPIO_PinOType				= GPIO_OTYPE_PUSHPULL;
 	handlerLED_2.GPIO_PinConfig.GPIO_PinPuPdControl			= GPIO_PUPDR_NOTHING;
 	handlerLED_2.GPIO_PinConfig.GPIO_PinSpeed				= GPIO_OSPEED_FAST;
@@ -113,7 +113,7 @@ int main(void)
 	// configurando el bit-2 puerto C (GPIOC) pin 7 PC7
 	handlerLED_3.pGPIOx										= GPIOC;
 	handlerLED_3.GPIO_PinConfig.GPIO_PinNumber				= PIN_7;
-	handlerLED_3.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_IN;
+	handlerLED_3.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
 	handlerLED_3.GPIO_PinConfig.GPIO_PinOType				= GPIO_OTYPE_PUSHPULL;
 	handlerLED_3.GPIO_PinConfig.GPIO_PinPuPdControl			= GPIO_PUPDR_NOTHING;
 	handlerLED_3.GPIO_PinConfig.GPIO_PinSpeed				= GPIO_OSPEED_FAST;
@@ -122,7 +122,7 @@ int main(void)
 	// Configurando el bit-3 puerto A (GPIOA) pin 6 PA6
 	handlerLED_4.pGPIOx										= GPIOA;
 	handlerLED_4.GPIO_PinConfig.GPIO_PinNumber				= PIN_6;
-	handlerLED_4.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_IN;
+	handlerLED_4.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
 	handlerLED_4.GPIO_PinConfig.GPIO_PinOType				= GPIO_OTYPE_PUSHPULL;
 	handlerLED_4.GPIO_PinConfig.GPIO_PinPuPdControl			= GPIO_PUPDR_NOTHING;
 	handlerLED_4.GPIO_PinConfig.GPIO_PinSpeed				= GPIO_OSPEED_FAST;
@@ -131,7 +131,7 @@ int main(void)
 	// Configurando el bit-4 puerto B (GPIOB) pin 8 PB8
 	handlerLED_5.pGPIOx										= GPIOB;
 	handlerLED_5.GPIO_PinConfig.GPIO_PinNumber				= PIN_8;
-	handlerLED_5.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_IN;
+	handlerLED_5.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
 	handlerLED_5.GPIO_PinConfig.GPIO_PinOType				= GPIO_OTYPE_PUSHPULL;
 	handlerLED_5.GPIO_PinConfig.GPIO_PinPuPdControl			= GPIO_PUPDR_NOTHING;
 	handlerLED_5.GPIO_PinConfig.GPIO_PinSpeed				= GPIO_OSPEED_FAST;
@@ -140,7 +140,7 @@ int main(void)
 	// Configurando el bit-5 puerto C (GPIOC) pin6 PC6
 	handlerLED_6.pGPIOx										= GPIOC;
 	handlerLED_6.GPIO_PinConfig.GPIO_PinNumber				= PIN_6;
-	handlerLED_6.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_IN;
+	handlerLED_6.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
 	handlerLED_6.GPIO_PinConfig.GPIO_PinOType				= GPIO_OTYPE_PUSHPULL;
 	handlerLED_6.GPIO_PinConfig.GPIO_PinPuPdControl			= GPIO_PUPDR_NOTHING;
 	handlerLED_6.GPIO_PinConfig.GPIO_PinSpeed				= GPIO_OSPEED_FAST;
@@ -149,7 +149,7 @@ int main(void)
 	// Configurando el bit-6 puerto C (GPIOC) pin 9 PC9
 	handlerLED_7.pGPIOx										= GPIOC;
 	handlerLED_7.GPIO_PinConfig.GPIO_PinNumber				= PIN_9;
-	handlerLED_7.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_IN;
+	handlerLED_7.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
 	handlerLED_7.GPIO_PinConfig.GPIO_PinOType				= GPIO_OTYPE_PUSHPULL;
 	handlerLED_7.GPIO_PinConfig.GPIO_PinPuPdControl			= GPIO_PUPDR_NOTHING;
 	handlerLED_7.GPIO_PinConfig.GPIO_PinSpeed				= GPIO_OSPEED_FAST;
@@ -169,11 +169,11 @@ int main(void)
 			contador += 1;
 		}
 
-		if(contador == 60){
-			contador = 0;
+		if(contador > 60){
+			contador = 1;
 		}
 
-		if(contador == 0){
+		if(contador < 1){
 			contador = 60;
 		}
 
@@ -191,17 +191,17 @@ void numBin(uint8_t contador){
 
 		valor1 = contador & 0b0000001;
 		valor2 = contador & 0b0000010;
-		valor2 = 1;
+		valor2 >>= 1;
 		valor3 = contador & 0b0000100;
-		valor3 = 2;
+		valor3 >>= 2;
 		valor4 = contador & 0b0001000;
-		valor4 = 3;
+		valor4 >>= 3;
 		valor5 = contador & 0b0010000;
-		valor5 = 4;
+		valor5 >>= 4;
 		valor6 = contador & 0b0100000;
-		valor6 = 5;
+		valor6 >>= 5;
 		valor7 = contador & 0b1000000;
-		valor7 = 6;
+		valor7 >>= 6;
 
 		if(valor1 == 1){
 			GPIO_WritePin(&handlerLED_1, SET);
